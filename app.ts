a3a556dfc2fe976f7ba5bd31410b021f4cdeac65
dubeyJi <ashd1014@hotmail.com>
2024-11-14 23:47:33 +0530

    using enviorment for cors

--- app.ts
+++ app.ts
@@ -11,7 +11,7 @@ dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.REACT_ORIGIN,
     credentials: true
 }));
 app.use(helmet())
