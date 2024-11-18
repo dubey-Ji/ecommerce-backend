461d621740e447794bebe8ee1c3b581667349497
dubeyJi <ashd1014@hotmail.com>
2024-11-18 21:51:24 +0530

    changing the middleware functionality to previous version

--- src/middleware/authentication.middleware.ts
+++ src/middleware/authentication.middleware.ts
@@ -6,43 +6,29 @@ export const authenticate = async (req: Request, res: Response, next: NextFuncti
   try {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
-        res.status(401).json({ message: 'Unauthorized, token missing' });
-        return;
+    res.status(401).json({ message: 'Unauthorized, token missing' });
     }
 
     const token = authHeader?.split(' ')[1];
     if (!token) {
         res.status(401).json({ message: 'Unauthorized, token missing' });
-        return;
     }
     // Ensure JWT_SECRET is defined
     if (!process.env.JWT_SECRET) {
         throw new Error("JWT_SECRET is not defined");
     }
     // Verify the token
-    let decoded: any;
-    try {
-        decoded = verify(token as string, process.env.JWT_SECRET as string) as unknown as { userId: string };
-    } catch (error) {
-        res.status(401).json({ message: 'Unauthorized, invalid token' });
-        return;
-    }
+    const decoded = verify(token as string, process.env.JWT_SECRET as string) as unknown as { userId: string };
 
     // Find the user
-    if (!decoded) {
-        res.status(401).json({ message: 'Unauthorized, invalid token' });
-        return;
-    }
     const user = await UserModel.findById(decoded.userId);
     if (!user) {
         res.status(401).json({ message: 'Unauthorized, user not found' });
-        return;
     }
 
     req.user = user;
     next();
   } catch (error) {
-    console.log('error', error);
     next(error);
   }
 };
